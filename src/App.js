f8e895a217ea80be334d3e2528b97d67e1bd0c18
Barbara Pagano <kotokokairapb@gmail.com>
2023-03-22 14:27:58 -0400

    este es el inicio

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: '100%', height: '100vh'}}>
+      asdsad
+      <iframe id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="100%"
+        height="100%"
+        // src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+        src="https://www.reuters.com/"
+      >
+      </iframe>
     </div>
   );
 }
